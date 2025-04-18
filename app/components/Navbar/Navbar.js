9eb50726d3a934b99c6a6e41e59ff374c183a1f7
https://github.com/Nikhil79004/BetterBlues <mehranikhilsingh@gmail.com>
2025-04-18 12:02:10 +0530

    added api

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -1,31 +1,3 @@
-// "use client"
-// import React from 'react'
-
-// const Navbar = () => {
-//   const navItems = ['Home', 'About', 'Latest Causes', 'Social Events', 'Blog', 'Contact'];
-
-//   return (
-//     <nav className='flex items-center justify-between p-6'>
-//       <div>
-//         <img src="/betterblues-logo.png" alt="BetterBlues Logo" className="h-20 w-auto" />
-//       </div>
-
-//       <div className='flex gap-8 items-center text-blue-900'>
-//         {navItems.map((item, index) => (
-//           <h3 key={index} className='text-lg font-medium cursor-pointer hover:text-fuchsia-700 transition'>
-//             {item}
-//           </h3>
-//         ))}
-
-//         <button className='bg-green-500 hover:bg-green-600 transition rounded-lg font-semibold text-lg px-7 py-2 text-white'>
-//           Donate
-//         </button>
-//       </div>
-//     </nav>
-//   )
-// }
-
-// export default Navbar;
 "use client"
 import React from 'react';
 
